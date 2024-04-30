380fd9be0ac0c92f5f2eb83812232b44aef8045c
wallacegibbon <wallacegibbon@aliyun.com>
2024-04-30 15:20:30 +0800

    Trivial improvement: The `cc_hash_map_iter` related code is now more elegant. (And is with some comments now)

--- src/cc_hash_map.c
+++ src/cc_hash_map.c
@@ -172,8 +172,8 @@ static int cc_hash_map_iter_step(struct cc_hash_map_iter *self)
 	struct cc_list_map **cursor;
 
 	while (1) {
-		if (cc_array_iter_next(&self->inner_array_iter, (void **)&cursor, NULL))
-			return 11;
+		if (cc_iter_next(&self->inner_array_iter, (void **)&cursor, NULL))
+			return -1;
 		if (*cursor != NULL)
 			break;
 	}
@@ -188,12 +188,12 @@ int cc_hash_map_iter_next(struct cc_hash_map_iter *self, void **item, size_t *in
 	if (self->is_empty)
 		return 2;
 
-	if (!cc_list_map_iter_next(&self->inner_list_map_iter, item, NULL))
+	if (!cc_iter_next(&self->inner_list_map_iter, item, NULL))
 		goto success;
 
 	if (cc_hash_map_iter_step(self))
 		return 3;
-	if (cc_list_map_iter_next(&self->inner_list_map_iter, item, NULL))
+	if (cc_iter_next(&self->inner_list_map_iter, item, NULL))
 		return 4;
 
 success:
@@ -207,6 +207,7 @@ success:
 int cc_hash_map_iter_init(struct cc_hash_map_iter *self, struct cc_hash_map *map)
 {
 	int tmp;
+
 	if (map == NULL)
 		return 1;
 	if (cc_array_iter_init(&self->inner_array_iter, map->data))
@@ -214,18 +215,18 @@ int cc_hash_map_iter_init(struct cc_hash_map_iter *self, struct cc_hash_map *map
 
 	self->iterator = &iterator_interface;
 	self->count = 0;
+	self->is_empty = 0;
 
 	/// `hash_map`'s `init` will call `step`, which is dangerous. Be careful with code here.
 	tmp = cc_hash_map_iter_step(self);
-	if (tmp == 0) {
-		self->is_empty = 0;
+	if (tmp == 0)
 		return 0;
-	} else if (tmp == 11) {
-		self->is_empty = 1;
-		return 0;
-	} else {
-		return 3;
-	}
 
+	/// unhandled error when calling `_step`
+	if (tmp != -1)
+		return 3;
+
+	/// set the flag so `_next` knows when it should return directly.
+	self->is_empty = 1;
 	return 0;
 }

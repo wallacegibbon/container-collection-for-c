cb2ddab8facc0330014b873134a8a750b1fbac76
wallacegibbon <wallacegibbon@aliyun.com>
2024-06-19 11:19:22 +0800

    Trivial changes: The `run-test.sh` is now copied from `miscellaneous-makefiles` along with the `cc-with-test.mk`.

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+# This script works with `cc-with-test.mk`. (But `cc-with-test.mk` is not used directly by this script)
 
 MAKE_OPT=""
 
@@ -12,7 +13,7 @@ parse_arguments() {
 }
 
 run_test() {
-	echo running test file "$1"...
+	echo [TEST] "$1"...
 	make $MAKE_OPT build/$(basename $1 .c) || exit 1
 	echo
 	echo
@@ -22,7 +23,7 @@ if test $# -eq 1; then
 	parse_arguments $1
 fi
 
-for f in test/*; do
+for f in test/*.c; do
 	run_test $f
 done
 
